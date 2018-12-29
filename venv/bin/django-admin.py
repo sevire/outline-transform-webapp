ce76b93fe4852444bd5dba64917cd2af155eb03d
Thomas Gaylard Admin <tbg-beanstalk@genonline.co.uk>
2018-12-29 17:12:39 +0000

    Initial commit after creating a base django web app project.

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/thomasdeveloper/Documents/Projects/outline_data_transform_webapp/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
